7f6b9089722d0feecead7d400efc137d4bfae58b
jesssubin <jess.s.moon@gmail.com>
2020-09-23 22:19:38 +0000

    assignment completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
-const wordSearch = (letters, word) => { 
+const transpose = require('../../d2/matrix_transposition'); 
+
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    for (const l of horizontalJoin) {
+        if (l.includes(word)){
+            return true; 
+        } 
     }
+   const verticalJoin = transpose(letters).map(ls => ls.join('')); 
+    for (const l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false; 
 }
 
 module.exports = wordSearch
